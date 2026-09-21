0053ba44451892ca3c6e9b0cf8869fb14883cc23
jaknilsc <jn@jaknils.com>
2026-09-21 20:59:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const memos=[
   '“The Cultural Programme confirms that pretentious theatre remains eligible for funding.”',
   '“Requests for conventional entertainment (cinema and salty popcorn) will be reviewed on a case-by-case basis.”',
   '“Coffee reserves have fallen below strategic levels. Finance has been authorised without further consultation.”',
-  '“Sashimi expenditure has been classified as relationship infrastructure rather than discretionary spending.”',
+  '“Sashimi expenditure has been classified as relationship infrastructure.”',
   '“The Danish Direct Debit Department denies allegations that its approval process consists entirely of Jake pressing Send in Revolut.”',
   '“All transfers marked ‘culturally important human’ are exempt from ordinary budget controls.”',
   '“The Care Team reminds personnel that tired human agents may require food/wine before meaningful conversation can resume.”',
