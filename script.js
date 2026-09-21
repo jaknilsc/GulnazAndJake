7e21ab3679071ea8e478a3319da49f9424bfbbbf
jaknilsc <jn@jaknils.com>
2026-09-21 21:00:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const memos=[
   '“Coffee reserves have fallen below strategic levels. Finance has been authorised without further consultation.”',
   '“Sashimi expenditure has been classified as relationship infrastructure.”',
   '“The Danish Direct Debit Department denies allegations that its approval process is Jake pressing Send in Revolut.”',
-  '“All transfers marked ‘culturally important human’ are exempt from ordinary budget controls.”',
+  '“All transfers marked ‘culturally important human’ are excluded from ordinary budget controls.”',
   '“The Care Team reminds personnel that tired human agents may require food/wine before meaningful conversation can resume.”',
   '“A temporary reduction in social battery is not a system outage. We love to charge.”',
   '“Rest has been approved by management. No productivity is required.”',
